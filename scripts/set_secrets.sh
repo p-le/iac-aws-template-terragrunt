a5450ebadc07ba62d78a010561a0cf73e19a16f2
Phu Le <phu.le.dev@gmail.com>
2022-03-29 03:38:51 +0000

    plan&validate workflow: add validate step, fix env.example

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -6,6 +6,7 @@ if [ -f "$FILE" ]; then
     export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
     gh secret set AWS_ACCESS_KEY_ID --body "$AWS_ACCESS_KEY_ID"
     gh secret set AWS_SECRET_ACCESS_KEY --body "$AWS_SECRET_ACCESS_KEY"
+    gh secret set AWS_DEFAULT_REGION --body "$AWS_DEFAULT_REGION"
 else
     echo "Please create .env file!"
 fi
