b6d4a78beb3639bb3239e63193474ae1aaf06fb5
Phu Le <phu.le.dev@gmail.com>
2022-03-29 04:55:48 +0000

    plan&validate workflow: add step to decode common_vars.yaml, add script to add secret COMMON_VARS_YAML

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-FILE=.env
-if [ -f "$FILE" ]; then
-    echo "Set GitHub Action Secrets"
+if [ -f ".env" ]; then
+    echo "Set GitHub Action Secrets from .env"
     export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
     gh secret set AWS_ACCESS_KEY_ID --body "$AWS_ACCESS_KEY_ID"
     gh secret set AWS_SECRET_ACCESS_KEY --body "$AWS_SECRET_ACCESS_KEY"
     gh secret set AWS_DEFAULT_REGION --body "$AWS_DEFAULT_REGION"
 else
     echo "Please create .env file!"
+fi
+
+if [ -f "infrastructure-live/common_vars.yaml" ]; then
+    echo "Set GitHub Action Secrets from common_vars.yaml"
+    base64 infrastructure-live/common_vars.yaml > infrastructure-live/common_vars.yaml.base64
+    gh secret set COMMON_VARS_YAML < infrastructure-live/common_vars.yaml.base64
+    rm -rf infrastructure-live/common_vars.yaml.base64
+else
+    echo "Please create common_vars.yaml file!"
 fi
